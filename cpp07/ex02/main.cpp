7dba66190360c916044411955e48a03ab04c85ef
Pedrito954 <pierre_giraudeau@hotmail.fr>
2023-11-10 00:06:43 +0100

    tests:ex02 cpp07

--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -5,70 +5,70 @@
 
 int main(int, char**)
 {
-    std::cout << "----------TEST MAIN INTRA---------" << std::endl << std::endl;
-    {
-        Array<int> numbers(MAX_VAL);
-        int* mirror = new int[MAX_VAL];
-        srand(time(NULL));
-        for (int i = 0; i < MAX_VAL; i++)
-        {
-            const int value = rand();
-            numbers[i] = value;
-            mirror[i] = value;
-        }
-        //SCOPE
-        {
-            Array<int> tmp = numbers;
-            Array<int> test(tmp);
-        }
+	std::cout << "----------TEST MAIN INTRA---------" << std::endl << std::endl;
+	{
+		Array<int> numbers(MAX_VAL);
+		int* mirror = new int[MAX_VAL];
+		srand(time(NULL));
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			const int value = rand();
+			numbers[i] = value;
+			mirror[i] = value;
+		}
+		//SCOPE
+		{
+			Array<int> tmp = numbers;
+			Array<int> test(tmp);
+		}
 
-        for (int i = 0; i < MAX_VAL; i++)
-        {
-            if (mirror[i] != numbers[i])
-            {
-                std::cerr << "didn't save the same value!!" << std::endl;
-                return 1;
-            }
-        }
-        try
-        {
-            numbers[-2] = 0;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << "Try acces wrong array pos [-2] -> " << e.what() << '\n';
-        }
-        try
-        {
-            numbers[MAX_VAL] = 0;
-        }
-        catch(const std::exception& e)
-        {
-            std::cerr << "Try acces wrong array pos [Max_VAL] -> " << e.what() << '\n';
-        }
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			if (mirror[i] != numbers[i])
+			{
+				std::cerr << "didn't save the same value!!" << std::endl;
+				return 1;
+			}
+		}
+		try
+		{
+			numbers[-2] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Try acces wrong array pos [-2] -> " << e.what() << '\n';
+		}
+		try
+		{
+			numbers[MAX_VAL] = 0;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Try acces wrong array pos [Max_VAL] -> " << e.what() << '\n';
+		}
 
-        for (int i = 0; i < MAX_VAL; i++)
-        {
-            numbers[i] = rand();
-        }
-        delete [] mirror;//
-    }
+		for (int i = 0; i < MAX_VAL; i++)
+		{
+			numbers[i] = rand();
+		}
+		delete [] mirror;//
+	}
 
-    std::cout << "----------MY TEST---------" << std::endl << std::endl;
-    {
-        Array<int> numbers(5);
+	std::cout << "----------MY TEST---------" << std::endl << std::endl;
+	{
+		Array<int> numbers(5);
 
-        for (int i = 0; i < 5; i++)
-        {
-            numbers[i] = 1440;
-        }
-        numbers[4] = 100;
-        std::cout << numbers<< std::endl;
+		for (int i = 0; i < 5; i++)
+		{
+			numbers[i] = 1440;
+		}
+		numbers[4] = 100;
+		std::cout << numbers<< std::endl;
 
-        Array<int> nbr(18);
-        nbr = numbers;
+		Array<int> nbr(18);
+		nbr = numbers;
 
-        std::cout << nbr;
-    }
+		std::cout << nbr;
+	}
 
 }
