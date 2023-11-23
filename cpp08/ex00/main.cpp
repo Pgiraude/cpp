c9bdf4cd80df971d96b58be2a362b468e1092415
Pierre GIRAUDEAU <pgiraude@made-f0Cr2s5.clusters.42paris.fr>
2023-11-23 15:34:32 +0100

    small modif

--- cpp08/ex00/main.cpp
+++ cpp08/ex00/main.cpp
@@ -2,33 +2,6 @@
 
 int	main(void)
 {
-	{
-		std::list<int> lst;
-		lst.push_back(10);
-		lst.push_front(9);
-		lst.push_back(444);
-
-		std::list<int>::const_iterator it;
-
-		for (it = lst.begin(); it != lst.end(); it++)
-			std::cout << *it << std::endl;
-
-		std::map<std::string, int> map1;
-		map1["lol"] = 14;
-		std::cout << map1["aaaaaaa"] << std::endl;
-
-		std::cout << "-----------" << std::endl;
-
-		std::vector<int>	v1(2);
-		v1.push_back(888);
-		v1.push_back(999);
-		for (long unsigned int i = 0; i < v1.size(); i++)
-			std::cout << v1[i] << std::endl;
-		int *ptr = v1.data();
-		std::cout << "-----------" << std::endl;
-
-		std::cout << *(ptr + 2)<< std::endl;
-	}
 	std::cout << "--------- Test n1 easyfind vector---------" << std::endl << std::endl;
 	{
 		std::vector<int>	v1(2);
@@ -46,5 +19,4 @@ int	main(void)
 		easyfind(l1, 999);
 		easyfind(l1, 2);
 	}
-
 }
